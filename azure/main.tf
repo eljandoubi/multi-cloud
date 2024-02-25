1d27cc6837e53f6a26af8bd2fd2343dc47457530
eljandoubi <abdelkarim.eljandoubi@ensta-paris.fr>
2024-02-25 12:47:05 +0100

    complete scripts

--- azure/main.tf
+++ azure/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_group" "udacity" {
   location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
   ip_address_type     = "Public"
-  dns_name_label      = "udacity-tscotto-azure"
+  dns_name_label      = "udacity-eljandoubi-azure"
   os_type             = "Linux"
 
   container {
@@ -16,8 +16,8 @@ resource "azurerm_container_group" "udacity" {
     cpu    = "0.5"
     memory = "1.5"
     environment_variables = {
-      "AWS_S3_BUCKET"       = "udacity-tscotto-aws-s3-bucket",
-      "AWS_DYNAMO_INSTANCE" = "udacity-tscotto-aws-dynamodb"
+      "AWS_S3_BUCKET"       = "udacity-eljandoubi-aws-s3-bucket",
+      "AWS_DYNAMO_INSTANCE" = "udacity-eljandoubi-aws-dynamodb"
     }
     ports {
       port     = 3000
@@ -30,3 +30,37 @@ resource "azurerm_container_group" "udacity" {
 }
 
 ####### Your Additions Will Start Here ######
+
+resource "azurerm_storage_account" "eljandoubi_storage_account" {
+  name                     = "eljandoubi-storage-account"
+  resource_group_name      = data.azurerm_resource_group.udacity.name
+  location                 = data.azurerm_resource_group.udacity.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_mssql_server" "nikosko_sql_server" {
+  name                         = "eljandoubi-sql-server"
+  resource_group_name          = data.azurerm_resource_group.udacity.name
+  location                     = data.azurerm_resource_group.udacity.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_service_plan" "nikosko_service_plan" {
+  name                = "eljandoubi-service-plan"
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  location            = data.azurerm_resource_group.udacity.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "nikosko_web_app" {
+  name                = "eljandoubi-web-app"
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  location            = data.azurerm_resource_group.udacity.location
+  service_plan_id     = azurerm_service_plan.nikosko_service_plan.id
+
+  site_config {}
+}
